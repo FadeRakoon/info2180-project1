8d3cdff107771fca628381a4e117fb93e92708ff
FadeRakoon <110274955+FadeRakoon@users.noreply.github.com>
2025-11-20 23:35:50 -0500

    added a sprinke of javascript; updated styles.css

--- scripts.js
+++ scripts.js
@@ -1,2 +1,19 @@
 /* Add your JavaScript to this file */
 
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector("form");
+    const emailInput = document.getElementById("email");
+    const message = document.querySelector(".message");
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const email = emailInput.value;
+
+        if (email){
+            message.textContent = `Thank you! Your email address '${email}' has been added to our mailing list!`;
+        }
+
+        emailInput.value ='';
+    });
+});
